ccfded8982ff8ea0212eca2d022de17239b76807
dakasakti <dakasakti.id@gmail.com>
2022-08-09 23:41:22 +0700

    fix static files

--- deliveries/middlewares/general.go
+++ deliveries/middlewares/general.go
@@ -6,6 +6,7 @@ import (
 )
 
 func General(e *echo.Echo) {
+	e.Static("/assets", "views/assets")
 	e.Use(middleware.CORS())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
